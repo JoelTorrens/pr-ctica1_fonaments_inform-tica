6c40bfc00acfca518feb3455cd4f3250bd224239
marmassanas <mar.massanas@autonoma.cat>
2024-10-07 09:58:08 +0200

    Càrrega eliminant registres amb errors

--- codiPr1.sh
+++ codiPr1.sh
@@ -1,4 +1,7 @@
 #!
 #!/bin/bash
 cut -d',' -f1-11,13-15 supervivents.csv > superviventsModificat1.csv
+awk -F',' '$14 != "True"' "superviventsModificat1.csv" > "superviventsModificat2.csv"
+eliminats=$(awk -F',' '$14 == "True"' "superviventsModificat1.csv" | wc -l)               
+echo "S'han eliminat $eliminats registres amb errors o vídeos esborrats."
 
