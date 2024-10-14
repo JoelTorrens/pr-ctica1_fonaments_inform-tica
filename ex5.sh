5c268529c4e91f89d808e9d51334e79ec8849ac8
marmassanas <mar.massanas@autonoma.cat>
2024-10-14 16:09:15 +0200

    carrega 14-oct

--- ex5.sh
+++ ex5.sh
@@ -3,15 +3,15 @@
 echo "Introdueix com a paràmetre d'entrada l'id del vídeo o el seu títol: "
 read parametre
 
-# Buscar coincidències a l'arxiu sortida.csv
-match=$(grep -i "$parametre" superviventsModificat4.csv)
+# Buscar coincidències a superviventsModificat4.csv a les columes $1 (video_id) i $3 (title)
+match=$(grep -i "$parametre" superviventsModificat4.csv | awk -F',' '$1 ~ /'"$parametre"'/ || $3 ~ /'"$parametre"'/')
 
 if [ -n "$match" ]; then
     # Si s'ha trobat una coincidència, mostrar els camps requerits
     # Processar la línia coincident
     echo "$match" | while IFS= read -r line; do
         # Utilitzar cut per extreure les columnes necessàries
-        title=$(echo "$line" | cut -d',' -f3)
+	title=$(echo "$line" | cut -d',' -f3)
         publish_time=$(echo "$line" | cut -d',' -f6)
         views=$(echo "$line" | cut -d',' -f8)
         likes=$(echo "$line" | cut -d',' -f9)
@@ -21,7 +21,7 @@ if [ -n "$match" ]; then
         Rdislikes=$(echo "$line" | cut -d',' -f17)
 
         # Mostrar els camps en format llegible
-        echo "Title: $title"
+	echo "Title: $title"
         echo "Publish time: $publish_time"
         echo "Views: $views"
         echo "Likes: $likes"
