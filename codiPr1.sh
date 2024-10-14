bce34075bb5f811e8f26834c4294efa0443f658e
marmassanas <mar.massanas@autonoma.cat>
2024-10-14 13:55:09 +0200

    carrega n

--- codiPr1.sh
+++ codiPr1.sh
@@ -67,3 +67,47 @@ chmod +x relacions.sh
 ./relacions.sh
 
 
+#5
+nano ex5.sh
+
+#!/bin/bash
+
+echo "Introdueix com a paràmetre d'entrada l'id del vídeo o el seu títol: "
+read parametre
+
+# Buscar coincidències del paràmetre a l'arxiu superviventsModificat4.csv
+match=$(grep -i "$parametre" superviventsModificat4.csv)
+
+if [ -n "$match" ]; then
+    # Si s'ha trobat una coincidència, mostrar els camps requerits
+    # Processar la línia coincident
+    echo "$match" | while IFS= read -r line; do
+        # Utilitzar cut per extreure les columnes necessàries
+        title=$(echo "$line" | cut -d',' -f3)
+        publish_time=$(echo "$line" | cut -d',' -f6)
+        views=$(echo "$line" | cut -d',' -f8)
+        likes=$(echo "$line" | cut -d',' -f9)
+        dislikes=$(echo "$line" | cut -d',' -f10)
+        Ranking_Views=$(echo "$line" | cut -d',' -f15)
+        Rlikes=$(echo "$line" | cut -d',' -f16)
+        Rdislikes=$(echo "$line" | cut -d',' -f17)
+
+        # Mostrar els camps en format llegible
+        echo "Title: $title"
+        echo "Publish time: $publish_time"
+        echo "Views: $views"
+        echo "Likes: $likes"
+        echo "Dislikes: $dislikes"
+        echo "Ranking Views: $Ranking_Views"
+        echo "Rlikes: $Rlikes"
+        echo "Rdislikes: $Rdislikes"
+        echo ""
+    done
+else
+    # Si no s'han trobat coincidències
+    echo "ERROR: '$parametre' NOT FOUND"
+fi
+
+
+chmod +x ex5.sh
+./ex5.sh
